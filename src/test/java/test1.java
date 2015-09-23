f9330a219bc95dfd6f5dfd8080bee6540bc22e46
cristina-sirbu <cristina.sirbu@info.uaic.ro>
2015-09-23 11:19:36 +0300

    Added test login
    Added a test to click on login

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,5 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 /**
  * Modified by Sergiu Vidrascu on 21/09/15.
  */
 public class test1 {
+	
+	@Test
+	public void clickLogin() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.pcgarage.ro/");
+		WebElement login=driver.findElement(By.xpath("//*[@id='user_header']//a[text()=\"Contul meu\"]"));
+		login.click();
+	}
 }
