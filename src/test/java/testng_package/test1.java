08e2131a4b67d5dc2fbd128bcce1153e9911e100
lazargeorge <lazargeorge@yahoo.com>
2016-01-08 16:38:53 +0200

    decomment tests from test1

--- src/test/java/testng_package/test1.java
+++ src/test/java/testng_package/test1.java
@@ -38,52 +38,52 @@ public class test1 extends Base {
 		Assert.assertTrue(waitForElement(test_login.msg_logins_ident).isDisplayed());
 	}
 
-//	@Test
-//	public void login_success() {
-//		LoginPage test_login = PageFactory.initElements(efdriver, LoginPage.class);
-//		test_login.log_in();
-//		Assert.assertTrue(waitForElement(test_login.contul_meu).isDisplayed());
-//	}
+	@Test
+	public void login_success() {
+		LoginPage test_login = PageFactory.initElements(efdriver, LoginPage.class);
+		test_login.log_in();
+		Assert.assertTrue(waitForElement(test_login.contul_meu).isDisplayed());
+	}
 
-//	@Test
-//	public void register_fail1() {
-//
-//		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
-//
-//		test_reg.register("ionut123", "pwdabc1", "pwdabc2", "ionut@yahoo.com", "ionut@yahoo.com", "Persoana Fizica",
-//				"1911030168964", "ion", "gheorghe", "strada abc, nr 3, Iasi", "0744567890");
-//		Assert.assertTrue(waitForElement(test_reg.msg_logins_confirm_password).isDisplayed());
-//
-//
-//	}
-//
-//	@Test
-//	public void register_fail2() {
-//
-//		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
-//
-//		test_reg.register("", "pwdabc1", "pwdabc2", "ionut@yahoo.com", "ionut@yahoo.com", "Persoana Fizica",
-//				"1911030168964", "ion", "gheorghe", "strada abc, nr 3, Iasi", "0744567890");
-//		Assert.assertTrue(waitForElement(test_reg.msg_logins_username).isDisplayed());
-//
-//	}
-//
-//	@Test
-//	public void register_success() {
-//
-//		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
-//
-//		test_reg.register();
-//		Assert.assertTrue(waitForElement(test_reg.iesire_cont).isDisplayed());
-//
-//	}
-//
-//	@Test
-//	public void try_test() {
-//
-//		efdriver.get("http://www.google.com/");
-//		Assert.assertTrue(true);
-//
-//	}
+	@Test
+	public void register_fail1() {
+
+		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
+
+		test_reg.register("ionut123", "pwdabc1", "pwdabc2", "ionut@yahoo.com", "ionut@yahoo.com", "Persoana Fizica",
+				"1911030168964", "ion", "gheorghe", "strada abc, nr 3, Iasi", "0744567890");
+		Assert.assertTrue(waitForElement(test_reg.msg_logins_confirm_password).isDisplayed());
+
+
+	}
+
+	@Test
+	public void register_fail2() {
+
+		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
+
+		test_reg.register("", "pwdabc1", "pwdabc2", "ionut@yahoo.com", "ionut@yahoo.com", "Persoana Fizica",
+				"1911030168964", "ion", "gheorghe", "strada abc, nr 3, Iasi", "0744567890");
+		Assert.assertTrue(waitForElement(test_reg.msg_logins_username).isDisplayed());
+
+	}
+
+	@Test
+	public void register_success() {
+
+		RegisterPage test_reg = PageFactory.initElements(efdriver, RegisterPage.class);
+
+		test_reg.register();
+		Assert.assertTrue(waitForElement(test_reg.iesire_cont).isDisplayed());
+
+	}
+
+	@Test
+	public void try_test() {
+
+		efdriver.get("http://www.google.com/");
+		Assert.assertTrue(true);
+
+	}
 
 }
