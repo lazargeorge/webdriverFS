44661bc2637555e1e4580086c288d4cda5af9e94
lazargeorge <lazargeorge@yahoo.com>
2016-01-08 15:15:48 +0200

    adding chrome test

--- src/test/java/testng_package/RunMe.java
+++ src/test/java/testng_package/RunMe.java
@@ -1,5 +1,7 @@
 package testng_package;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class RunMe {
@@ -8,4 +10,16 @@ public class RunMe {
 	public void start(){
 		
 	}
+	
+	@Test
+	public void googleSearch()
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+	
+		driver.get("http://google.com");
+		driver.quit();
+	}
+	
 }
